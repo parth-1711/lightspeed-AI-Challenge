ffd05637fe72d7a70d1480ac27c66f7e18f96aa6
Parth <parthirache8@gmail.com>
2023-12-10 13:09:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+app.get('/sayHello',(req,res)=>{
+    res.json({message:"Hello User."})
+})
+
+app.listen(80,()=>{
+    console.log('Server is up aand running on port 80 !');
+})
