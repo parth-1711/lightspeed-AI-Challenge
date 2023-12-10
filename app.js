20537251119f2c99de7a55303e82637233af6b31
Parth <parthirache8@gmail.com>
2023-12-10 17:16:41 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express=require('express');
-const bodyParser=require('body-parser');
+// const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.json());
 
 app.get('/sayHello',(req,res)=>{
     res.json({message:"Hello User."})
